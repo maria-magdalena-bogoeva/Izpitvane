1f1e797f9509d8399504a96296c36926e9327e96
maria-magdalena-bogoeva <maggiiiiii@abv.bg>
2022-10-21 09:59:56 +0300

    StudentSystemIzpitvane

---
+++ StudentSystem/StudentSystem/ContentType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystem
+{
+    public enum ContentType
+    { 
+        Application=1,
+        Pdf =2,
+        Zip=3
+    
+    }
+}
---
+++ StudentSystem/StudentSystem/Homework.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystem
+{
+    public class Homework
+    {
+       
+        public int HomeworkId { get; set; }
+
+
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public ContentType ContentType { get; set; }
+        [Required]
+        public DateTime SubmissionTime { get; set; }
+        [Required]
+        public int StudentId { get; set; }
+        [Required]
+        public Student Student { get; set; }
+        [Required]
+        public int CourseId { get; set; }
+        [Required]
+        public Course Course { get; set; }
+
+    }
+}
